fccc05e6ba14c4e4d73687e69c00047dad3b1b6b
GoopSoftware <jhreschke@gmail.com>
2025-05-27 13:38:59 -0600

    Created IntentManager class

---
+++ app/src/main/java/com/dzl/registrationform/IntentManager.kt
@@ -0,0 +1,19 @@
+package com.dzl.registrationform
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class IntentManager(
+    val firstName: String,
+    val lastName: String,
+    val title: String,
+    val email: String,
+    val phoneNumber: String,
+    val password: String
+) : Parcelable
+
+{
+
+
+}
--- app/src/main/java/com/dzl/registrationform/MainActivity.kt
+++ app/src/main/java/com/dzl/registrationform/MainActivity.kt
@@ -18,13 +18,30 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val titles: Array<String> = arrayOf("Mr.", "Ms", "Mrs", "Jr")
+        val titles: Array<String> = arrayOf("None", "Mr.", "Ms", "Mrs", "Jr")
         val adapter = ArrayAdapter(
             this,
              android.R.layout.simple_list_item_1,
-            titles
+             titles
         )
         binding.autoCompleteTitle.setAdapter(adapter)
+
+
+
+
+    }
+
+    private fun onAccountCreate() {
+
+        val summary = IntentManager(
+            binding.editTextFirstName.text.toString(),
+            binding.editTextLastName.text.toString(),
+            binding.autoCompleteTitle.text.toString(),
+            binding.editTextEmail.text.toString(),
+            binding.editTextPhoneNumber.text.toString(),
+            binding.editTextPassword.text.toString()
+        )
+
 
     }
 }
