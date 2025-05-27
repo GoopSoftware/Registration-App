f27ab13aa6a12ca3af0b75438e0b5ddf6f309877
GoopSoftware <jhreschke@gmail.com>
2025-05-26 21:38:49 -0600

    Styled the XML and started coding the spinner

--- app/src/main/java/com/dzl/registrationform/MainActivity.kt
+++ app/src/main/java/com/dzl/registrationform/MainActivity.kt
@@ -1,15 +1,30 @@
 package com.dzl.registrationform
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.dzl.registrationform.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val titles: Array<String> = arrayOf("Mr.", "Ms", "Mrs", "Jr")
+        val adapter = ArrayAdapter(
+            this,
+             android.R.layout.simple_list_item_1,
+            titles
+        )
+        binding.autoCompleteTitle.setAdapter(adapter)
 
     }
 }
